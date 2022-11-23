ae143449e6aab3c77a68901108f5ddd07f117c35
Funky26 <103521746+Funky26@users.noreply.github.com>
2022-11-23 22:10:55 +0100

    singletone,Addlistener

--- Assets/scripts/AnswereQuestion.cs
+++ Assets/scripts/AnswereQuestion.cs
@@ -1,44 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AnswereQuestion : MonoBehaviour
 {
     public GameObject gm;
     public GameObject smileyFace;
+    public Button True;
+    public Button False;
 
 
     public int questionCount = 5;
     public int answeredQuestions = 0;
     public int rightAnswers = 0;
 
-    public void OnClick()
+
+
+    public static AnswereQuestion Instance;
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+    void Start()
     {
         answeredQuestions++;
-        if(smileyFace.activeSelf)
-        {
-            rightAnswers++;
-        }
-        if(answeredQuestions== questionCount)
+    }
+
+    void Update()
+    {
+        True.onClick.AddListener(QuestionCount);
+        False.onClick.AddListener(QuestionCount);
+        
+
+        if (answeredQuestions == questionCount)
         {
             Destroy(gm);
             SceneManager.LoadScene("PlayAgain");
         }
-        DontDestroyOnLoad(this.gameObject);
     }
 
-
-    //*mergnut* gameManager script a urobit to podla UserSelect funkcie
-
-    void Start()
+    void QuestionCount()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (smileyFace.activeSelf)
+        {
+            rightAnswers++;
+        }
     }
 }
